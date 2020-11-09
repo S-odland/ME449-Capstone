d165078dc829cd44c2aa75b498e085a9bf2ba1ad
S-odland <49540258+S-odland@users.noreply.github.com>
2020-11-09 17:35:27 -0600

    initial conditions and inputs to the function all set up

--- TrajectoryGenerator.py
+++ TrajectoryGenerator.py
@@ -2,6 +2,7 @@
 
 import modern_robotics as mr 
 import numpy as np
+import math as m 
 
 ## This file generates a reference trajectory for the end-effector frame {e}. The trajectory consists of 
 ## eight concatenated trahectory segments beginning and ending at rest.
@@ -24,3 +25,23 @@ import numpy as np
 ## --------> [r11, r12, r13, r21, r22, r23, r31, r32, r33, px, py, pz, gripper state]
 
 ## This code should use either mr.ScrewTrajectory or mr.CartesianTrajectory to generate individual trajectory segments
+
+## A reference trajectory consists of eight segments:
+##    1. A trajectory to move the gripper from its initial configuration to a "standoff" configuration a few cm above the block
+##    2. A trajectory to move the gripper down to the grasp position
+##    3. Closing of the gripper
+##    4. A trajectory to move the gripper back to the "standoff" configuration 
+##    5. A trajectpry to move the gripper to a "standoff" confugration above the final configuration
+##    6. A trajectory to move the gripper tothe final configuration of the object
+##    7. Opening of the gripper
+##    8. A trajectory to move the gripper back to the "standoff" configuration
+
+Tsc_initial = np.array([[1,0,0,1],[0,1,1,0],[0,0,1,0.025],[0,0,0,2]]) # initial configuration of cube
+Tsc_final = np.array([[0,1,0,0],[-1,0,0,-1],[0,0,1,0.025],[0,0,0,1]]) # final configuration of cube
+
+Tse_initial = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0.5],[0,0,0,1]]) # initial configuration of the gripper
+Tce_standoff = np.array([[-0.7071,0,-0.7071,1],[0,1,0,0],[-0.7071,0,-0.7071,0.1],[0,0,0,1]])
+Tce_grip = np.array([[-0.7071,0,-0.7071,1],[0,1,0,0],[-0.7071,0,-0.7071,0.025],[0,0,0,1]])
+
+grip_state = 0
+
