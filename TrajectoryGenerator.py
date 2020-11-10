e73a1394a23bd61c6f88b3699e27684ea47447f3
S-odland <49540258+S-odland@users.noreply.github.com>
2020-11-10 10:08:45 -0600

    simulation works, just had to hard code initial configurations of the end effector in the space frame

--- TrajectoryGenerator.py
+++ TrajectoryGenerator.py
@@ -38,42 +38,37 @@ import math as m
 
 ## scene duration total 4+1+1+1+8+1+1+1 = 18 = N
 
-def configInCtoSE3(Tce_standoff,Tce_grip,Tsc_initial):
-
-    Tse_standoff = np.matmul(Tsc_initial,Tce_standoff)
-    Tse_grip = np.matmul(Tsc_initial,Tce_grip)
-
-    return Tse_standoff,Tse_grip
-
-def TrajectoryGenerator():
+def InitTG():
 
     eOffset = 0.075
 
-    Tsc_initial = np.array([[1,0,0,1],[0,1,1,0],[0,0,1,0.025],[0,0,0,2]]) # initial configuration of cube
-    Tsc_final = np.array([[0,1,0,0],[-1,0,0,-1],[0,0,1,0.025],[0,0,0,1]]) # final configuration of cube
+    # Tsc_initial = np.array([[1,0,0,1],[0,1,1,0],[0,0,1,0.025],[0,0,0,2]]) # initial configuration of cube
+    # Tsc_final = np.array([[0,1,0,0],[-1,0,0,-1],[0,0,1,0.025],[0,0,0,1]]) # final configuration of cube
 
     Tse_initial = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0.5],[0,0,0,1]]) # initial configuration of the gripper
 
-    Tce_standoff = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,eOffset],[0,0,0,1]]) # standoff configuration 
-    Tce_grip = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,0],[0,0,0,1]])
+    # Tce_standoff = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,eOffset],[0,0,0,1]]) # standoff configuration 
+    # Tce_grip = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,0],[0,0,0,1]])
 
-    grip_states = 0
-
-    grip_states = np.array([0])
-    trajectories = np.array([Tse_initial[0][0],Tse_initial[0][1],Tse_initial[0][2],Tse_initial[1][0],Tse_initial[1][1],\
-                            Tse_initial[1][2],Tse_initial[2][0],Tse_initial[2][1],Tse_initial[2][2],Tse_initial[0][3],\
-                            Tse_initial[1][3],Tse_initial[2][3],grip_states[0]])
-
-    Tse_standoffInit,Tse_gripInit = configInCtoSE3(Tce_standoff,Tce_grip,Tsc_initial)
+    Tse_standoffInit = np.array([[-0.7071,0,0.7071,1],[0,1,0,0],[-0.7071,0,-0.7071,eOffset+0.025],[0,0,0,1]])
+    Tse_gripInit = np.array([[-0.7071,0,0.7071,1],[0,1,0,0],[-0.7071,0,-0.7071,0.025],[0,0,0,1]])
 
     Tse_standoffFinal = np.array([[0,1,0,0],[0.7071,0,-0.7071,-1],[-0.7071,0,-0.7071,eOffset+0.025],[0,0,0,1]])
     Tse_gripFinal = np.array([[0,1,0,0],[0.7071,0,-0.7071,-1],[-0.7071,0,-0.7071,0.025],[0,0,0,1]])
 
+    return Tse_initial,Tse_standoffInit,Tse_gripInit,Tse_standoffFinal,Tse_gripFinal
+
+def TrajectoryGenerator():
+
+    Tse_initial,Tse_standoffInit,Tse_gripInit,Tse_standoffFinal,Tse_gripFinal = InitTG()
+
     traj_iter = np.array([Tse_initial,Tse_standoffInit,Tse_gripInit,Tse_gripInit,Tse_standoffInit,Tse_standoffFinal, \
                                                                 Tse_gripFinal,Tse_gripFinal, Tse_standoffFinal])
     
+    grip_states = 0
     t = np.array([4,1,1,1,8,1,1,1]) # durations for each segment of the total trajectory
-    traj_states = []
+    trajectories = []
+    gState = 0
     
     for i in range(0,len(traj_iter)-1):
 
@@ -82,29 +77,32 @@ def TrajectoryGenerator():
         Tf = t[i]
         N = Tf/0.01
 
+        if i == 2 or i == 6:
+            gState = not gState
+
         traj = mr.ScrewTrajectory(Xstart,Xend,Tf,N,3)
 
         for i in range(0,len(traj)):
 
-            if i == 2 or i == 6:
+            if gState:
                 grip_states = np.vstack([grip_states,1])
             else:
                 grip_states = np.vstack([grip_states,0])
 
-            traj_states.append(traj[i])
+            trajectories.append(traj[i])
     
     f = open("trajectory.csv", "w") 
     
-    for i in range(0,len(traj_states)):
+    for i in range(0,len(trajectories)):
 
-        T = traj_states[i]
+        T = trajectories[i]
         trajectory = "%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%d\n" % \
                                         (T[0][0],T[0][1],T[0][2],T[1][0],T[1][1],T[1][2],T[2][0],T[2][1],\
                                           T[2][2],T[0][3],T[1][3],T[2][3],grip_states[i])
 
         f.write(trajectory)
     f.close()
- 
 
 
+TrajectoryGenerator()
 
