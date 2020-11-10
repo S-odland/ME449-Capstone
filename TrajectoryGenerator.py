0616ef841708f278832b3cddc97e17f4f3faa5e3
S-odland <49540258+S-odland@users.noreply.github.com>
2020-11-09 20:15:12 -0600

    code is working, time to test in simulation

--- TrajectoryGenerator.py
+++ TrajectoryGenerator.py
@@ -38,7 +38,7 @@ import math as m
 
 ## scene duration total 4+1+1+1+8+1+1+1 = 18 = N
 
-def configInCtoSE3(Tce_standoff,Tce_grip):
+def configInCtoSE3(Tce_standoff,Tce_grip,Tsc_initial):
 
     Tse_standoff = np.matmul(Tsc_initial,Tce_standoff)
     Tse_grip = np.matmul(Tsc_initial,Tce_grip)
@@ -46,7 +46,7 @@ def configInCtoSE3(Tce_standoff,Tce_grip):
     return Tse_standoff,Tse_grip
 
 def TrajectoryGenerator():
-    
+
     eOffset = 0.075
 
     Tsc_initial = np.array([[1,0,0,1],[0,1,1,0],[0,0,1,0.025],[0,0,0,2]]) # initial configuration of cube
@@ -57,15 +57,14 @@ def TrajectoryGenerator():
     Tce_standoff = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,eOffset],[0,0,0,1]]) # standoff configuration 
     Tce_grip = np.array([[-0.7071,0,-0.7071,0],[0,1,0,0],[0.7071,0,-0.7071,0],[0,0,0,1]])
 
-    grip_state = 0
+    grip_states = 0
 
-    traj_states = np.array(Tse_initial.copy())
     grip_states = np.array([0])
     trajectories = np.array([Tse_initial[0][0],Tse_initial[0][1],Tse_initial[0][2],Tse_initial[1][0],Tse_initial[1][1],\
                             Tse_initial[1][2],Tse_initial[2][0],Tse_initial[2][1],Tse_initial[2][2],Tse_initial[0][3],\
                             Tse_initial[1][3],Tse_initial[2][3],grip_states[0]])
 
-    Tse_standoffInit,Tse_gripInit = configInCtoSE3(Tce_standoff,Tce_grip)
+    Tse_standoffInit,Tse_gripInit = configInCtoSE3(Tce_standoff,Tce_grip,Tsc_initial)
 
     Tse_standoffFinal = np.array([[0,1,0,0],[0.7071,0,-0.7071,-1],[-0.7071,0,-0.7071,eOffset+0.025],[0,0,0,1]])
     Tse_gripFinal = np.array([[0,1,0,0],[0.7071,0,-0.7071,-1],[-0.7071,0,-0.7071,0.025],[0,0,0,1]])
@@ -74,8 +73,9 @@ def TrajectoryGenerator():
                                                                 Tse_gripFinal,Tse_gripFinal, Tse_standoffFinal])
     
     t = np.array([4,1,1,1,8,1,1,1]) # durations for each segment of the total trajectory
+    traj_states = []
     
-    for i in range(0,len(traj_iter-1)):
+    for i in range(0,len(traj_iter)-1):
 
         Xstart = traj_iter[i]
         Xend = traj_iter[i+1]
@@ -84,32 +84,27 @@ def TrajectoryGenerator():
 
         traj = mr.ScrewTrajectory(Xstart,Xend,Tf,N,3)
 
-        for i in range(1,len(traj)):
+        for i in range(0,len(traj)):
 
             if i != 2 or i != 6:
                 grip_states = np.vstack([grip_states,0])
             else:
                 grip_states = np.vstack([grip_states,1])
 
-        traj_states = np.vstack([traj_states,traj[1:len(traj)-1,:]])
+            traj_states.append(traj[i])
     
-    for i in range(1,len(traj_states)):
+    f = open("trajectory.csv", "w") 
+    
+    for i in range(0,len(traj_states)):
 
         T = traj_states[i]
-        t = np.array([T[0][0],T[0][1],T[0][2],T[1][0],T[1][1],T[1][2],T[2][0],T[2][1],\
-                       T[2][2],T[0][3],T[1][3],T[2][3],grip_states[i]])
-
-        trajectories = np.vstack([trajectories,t])
-
-    f = open("trajectory.csv", "w") 
-
-    for i in range(len(trajectories)):
         trajectory = "%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%7.6f,%d\n" % \
-                                                    (trajectories[i][0],trajectories[i][1],trajectories[i][2],trajectories[i][3],\
-                                                    trajectories[i][4],trajectories[i][5],trajectories[i][6],trajectories[i][7],\
-                                                    trajectories[i][8],trajectories[i][9],trajectories[i][10],trajectories[i][11],\
-                                                    trajectories[i][12])
-        f.write(trajectory)
+                                        (T[0][0],T[0][1],T[0][2],T[1][0],T[1][1],T[1][2],T[2][0],T[2][1],\
+                                          T[2][2],T[0][3],T[1][3],T[2][3],grip_states[i])
 
+        f.write(trajectory)
     f.close()
+ 
+
+
 
